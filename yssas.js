e2744e01ac31067b678b6f8d2c63cf40e1707e23
Mary Karroqe <mkarroqe@gmail.com>
2015-08-29 18:32:21 -0400

    Create yssas.js

---
+++ yssas.js
@@ -0,0 +1,13 @@
+/*
+	 /\
+ 	/__\   ooh triangle
+
+	personal website????????? personal website.
+	by ya girl Mary Karroqe
+*/
+function explainy() {
+	document.getElementById("plain").innerHTML = "(kah-rOh-CHeh)";
+}
+function plainy() {
+	document.getElementById("plain").innerHTML = "Karroqe";
+}
